930d9ec53eb82c92fc0ba68ee69d2e37d608a75c
Victor VERHILLE <victor.verhille@diginext.fr>
2022-12-06 12:43:37 +0100

    day2 day5

---
+++ AoC2022/Days/Day2.cs
@@ -0,0 +1,120 @@
+﻿using AoC2022.Interfaces;
+using AoC2022.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2022.Days
+{
+    public enum RPS
+    {
+        Rock = 0,
+        Paper = 1,
+        Scissor = 2
+    }
+
+    public static class RPSExtensions
+    {
+        public static RPS GetDraw(this RPS pRPS)
+        {
+            return pRPS;
+        }
+
+        public static RPS GetLosingRPS(this RPS pRPS)
+        {
+            return (RPS)(((int)pRPS + 1) % 3);
+        }
+
+        public static RPS GetWinningRPS(this RPS pRPS)
+        {
+            return (RPS)(((int)pRPS + 2) % 3);
+        }
+    }
+
+    public class Day2 : IDay
+    {
+        #region Fields
+
+        List<Tuple<RPS, string>> mBattles = new List<Tuple<RPS, string>>();
+
+        # endregion Fields
+
+        #region Methods
+
+        public string GetFirstPuzzle()
+        {
+            return this.GetScore(this.mBattles.Select(pBattle => this.GetRPS1(pBattle))).ToString();
+        }
+
+        public string GetSecondPuzzle()
+        {
+            return this.GetScore(this.mBattles.Select(pBattle => this.GetRPS2(pBattle))).ToString();
+        }
+
+        public void ComputesData()
+        {
+            IEnumerable<string> lInput = Utils.GetInputData(this);
+            foreach (string lBattle in lInput)
+            {
+                string[] lChoices = lBattle.Split(' ');
+                this.mBattles.Add(new Tuple<RPS, string>(this.GetFirstPlayerGame(lChoices[0]), lChoices[1]));
+            }
+        }
+
+        private RPS GetFirstPlayerGame(string pPlay)
+        {
+            if (pPlay.Equals("A"))
+                return RPS.Rock;
+            if (pPlay.Equals("B"))
+                return RPS.Paper;
+            return RPS.Scissor;
+        }
+
+        private Tuple<RPS, RPS> GetRPS1(Tuple<RPS, string> pBattle)
+        {
+            RPS lWhatToPlay = RPS.Scissor;
+            if (pBattle.Item2.Equals("X"))
+                lWhatToPlay = RPS.Rock;
+            if (pBattle.Item2.Equals("Y"))
+                lWhatToPlay = RPS.Paper;
+            return new Tuple<RPS, RPS>(pBattle.Item1, lWhatToPlay);
+        }
+
+        private Tuple<RPS, RPS> GetRPS2(Tuple<RPS, string> pBattle)
+        {
+            RPS lWhatToPlay = pBattle.Item1.GetDraw();
+            if (pBattle.Item2 == "X")
+            {
+                lWhatToPlay = pBattle.Item1.GetWinningRPS();
+            }
+            if (pBattle.Item2 == "Z")
+            {
+                lWhatToPlay = pBattle.Item1.GetLosingRPS();
+            }
+            return new Tuple<RPS, RPS>(pBattle.Item1, lWhatToPlay);
+        }
+
+        private int GetVictoryPoints(Tuple<RPS, RPS> pBattle)
+        {
+            if (pBattle.Item2 == pBattle.Item1.GetDraw())
+                return 3;
+            if (pBattle.Item2 == pBattle.Item1.GetWinningRPS())
+                return 0;
+            return 6;
+        }
+
+        private int GetPoint(RPS pValue)
+        {
+            return (int)pValue + 1;
+        }
+
+        private int GetScore(IEnumerable<Tuple<RPS, RPS>> pRounds)
+        {
+            return pRounds.Select(pBattle => this.GetVictoryPoints(pBattle) + this.GetPoint(pBattle.Item2)).Sum();
+        }
+
+        #endregion Methods
+    }
+}
---
+++ AoC2022/Days/Day5.cs
@@ -0,0 +1,118 @@
+﻿using AoC2022.Interfaces;
+using AoC2022.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2022.Days
+{
+    public class Day5 : IDay
+    {
+        #region Fields
+
+        List<Tuple<int, int, int>> mInstructions = new List<Tuple<int, int, int>>();
+        List<Stack<string>> mStacks = new List<Stack<string>>();
+
+        # endregion Fields
+
+        #region Methods
+
+        public string GetFirstPuzzle()
+        {
+            foreach (Tuple<int,int,int> lInstruction in this.mInstructions)
+            {
+                for (int lIndex = 0; lIndex < lInstruction.Item1; lIndex++)
+                {
+                    this.mStacks.ElementAt(lInstruction.Item3 - 1).Push(this.mStacks.ElementAt(lInstruction.Item2 - 1).Pop());
+                }
+            }
+            return this.GetTopResult();
+        }
+
+        public string GetSecondPuzzle()
+        {
+            this.mInstructions.Clear();
+            this.mStacks.Clear();
+            this.ComputesData();
+            List<string> lTemp = new List<string>();
+            foreach (Tuple<int, int, int> lInstruction in this.mInstructions)
+            {
+                lTemp.Clear();
+                for (int lIndex = 0; lIndex < lInstruction.Item1; lIndex++)
+                {
+                    lTemp.Add(this.mStacks.ElementAt(lInstruction.Item2 - 1).Pop());
+                }
+                lTemp.Reverse();
+                foreach(string lCrate in lTemp)
+                {
+                    this.mStacks.ElementAt(lInstruction.Item3 - 1).Push(lCrate);
+                }
+            }
+            return this.GetTopResult();
+        }
+
+        public void ComputesData()
+        {
+            IEnumerable<string> lInput = Utils.GetInputData(this);
+            bool lIsIntruction = false;
+            int lNumberOfStacks = (lInput.First().Length + 1) / 4;
+            for (int lIndex = 0; lIndex < lNumberOfStacks; lIndex++)
+            {
+                this.mStacks.Add(new Stack<string>());
+            }
+            List<string> lStacks = new List<string>();
+            foreach (string lLine in lInput)
+            {
+                if (string.IsNullOrEmpty(lLine))
+                {
+                    lIsIntruction = true;
+                    lStacks.Reverse();
+                    lStacks.RemoveAt(0);
+                    foreach (string lStackLine in lStacks)
+                    {
+                        for (int lIndex = 0; lIndex < lNumberOfStacks; lIndex++)
+                        {
+                            char lChar = lStackLine[lIndex * 4 + 1];
+                            if (!lChar.Equals(' '))
+                            {
+                                this.mStacks.ElementAt(lIndex).Push(lChar.ToString());
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (lIsIntruction)
+                    {
+                        string[] lSplit = lLine.Split(' ');
+                        int lNumberOfCrates;
+                        int lFrom;
+                        int lTo;
+                        int.TryParse(lSplit[1], out lNumberOfCrates);
+                        int.TryParse(lSplit[3], out lFrom);
+                        int.TryParse(lSplit[5], out lTo);
+                        this.mInstructions.Add(new Tuple<int, int, int>(lNumberOfCrates, lFrom, lTo));
+                    }
+                    else
+                    {
+                        lStacks.Add(lLine);
+                    }
+                }
+            }
+        }
+
+        private string GetTopResult()
+        {
+            string lResult = "";
+            foreach (Stack<string> lStack in this.mStacks)
+            {
+                lResult += lStack.First();
+            }
+            return lResult;
+        }
+
+        #endregion Methods
+    }
+}
